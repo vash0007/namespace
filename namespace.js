1271666047de9943f5bc2d949ccdab8ed190e4d7
viraj vashi <vash0006@algonquinlive.com>
2018-10-26 11:42:20 -0400

    change

---
+++ namespace.js
@@ -0,0 +1,28 @@
+var VIRAJVASHI = {
+    init: function () {
+
+        let Box = document.createElement("div");
+        Box.className = "box";
+        Box.textContent = "virajvashi";
+        let Boxes = document.getElementById("boxes");
+        Boxes.appendChild(Box);
+
+        Box.addEventListener("click", function () {
+            Box.style.borderColor = "Black";
+            Box.style.backgroundColor = "Blue";
+            //Box.textContent = "virajvashi";
+        });
+
+
+        Box.addEventListener("mouseover", function () {
+            Box.classList.toggle("highlight");
+        });
+        Box.addEventListener("mouseout", function () {
+           Box.classList.toggle("highlight");
+        });
+
+
+
+
+    }
+};
